6e328e98e7d420bf52c68a2c3899776b8dd8a1f4
setayesh <setayshkarampoor623@gmail.com>
2025-08-19 22:47:01 +0330

    library izitoast js

---
+++ izitoast js/js/app.js
@@ -0,0 +1,8 @@
+iziToast.show({
+    title: 'hello',
+    message: 'This is library iziToast!!!',
+    theme: "dark",
+    position: "topRight",
+    timeout: 7000,
+    progressBarColor: '#000',
+});
