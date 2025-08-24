d184fdf7008a0472f66e9ffc41c9906a392e38bd
setayesh <setayshkarampoor623@gmail.com>
2025-08-24 10:46:53 +0330

    library slick carousel js

---
+++ slick carousel js/js/app.js
@@ -0,0 +1,30 @@
+
+$('.slick').slick({
+    autoplaySpeed: 3000,
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    lazyLoad: 'ondemand',
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }]
+});
